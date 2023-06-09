89ddef53b0cb10e3d170aea999849954dd959603
Saim Shahzad <saimshehzad2030@gmail.com>
2023-06-10 00:58:28 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express=require("express")
+const app=express()
+const port=3000
+
+
+app.get('/',(req,res)=>{
+
+    res.send("hello world");
+
+})
+
+
+
+app.get('/about',(req,res)=>{
+
+    res.send("hello from Mirza Saim Shahzad")
+})
+
+app.get('/contact',(req,res)=>{
+
+    res.send(`<h1>welcome to contact page buddy</h1>`)
+})
+
+app.get('/homepage',(req,res)=>{
+
+    res.send("<h1>welcome to homepage</h1>")
+})
+
+
+
+
+app.listen(port,()=>{
+
+    console.log(`listing at port ${port}...`)
+})
